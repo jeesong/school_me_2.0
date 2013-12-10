da0fe1afd3ec2120b90bf5973e80cc963550abb2
jeesong <jeewsong@gmail.com>
2013-12-10 09:50:39 -0800

    Started new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SchoolMe20::Application.config.secret_token = '920e36383f5791fadf5c5f1dc7e5cfabd5153835b5c340b1fee4acf2bdade2c690d926770d1607c1c29f95272d1b7dd51b20d17df432d868c7856487733591bf'
